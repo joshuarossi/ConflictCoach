9d2d10044cf2bce0c95afdc696d2ad010e03ac9e
Josh <maximojoshuarossi@gmail.com>
2026-05-01 12:25:29 -0600

    doing this to get all the generated stuff in

--- convex/_generated/api.ts
+++
@@ -1,6 +0,0 @@
-// Placeholder for Convex generated API types.
-// This file will be replaced when `npx convex dev` runs.
-
-/* eslint-disable @typescript-eslint/no-explicit-any */
-export const api: any = {};
-export const internal: any = {};
--- convex/_generated/dataModel.ts
+++
@@ -1,8 +0,0 @@
-// Placeholder for Convex generated data model types.
-// This file will be replaced when `npx convex dev` runs.
-
-export type Id<TableName extends string> = string & { __tableName: TableName };
-export type Doc<TableName extends string> = Record<string, unknown> & {
-  _id: Id<TableName>;
-  _creationTime: number;
-};
